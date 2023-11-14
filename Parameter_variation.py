52d32cf9b663c2c8659b0064ea9ed1d6aacec55e
Avanbreukeleng <avanbreukeleng@student.tudelft.nl>
2023-11-14 18:02:50 +0100

    Added new file for varying N and the different parameters. Debugging and animation

---
+++ Parameter_variation.py
@@ -0,0 +1,41 @@
+import numpy as np
+from main import Bird
+import matplotlib.pyplot as plt
+
+#list of the parameters in a Px8 matrix where P is the number of sets of parameters
+#to select one set of parameters select InParam[:,X] where x is one of the columns (=set of param)
+#A set of param is of the form np.array (seed, vel, N, R, L, eta, dt, Nstep)
+
+
+InParam = np.array(1, 0.033, 1, 1, 10, np.pi/4, 1, 10) #first set of parameters
+
+
+# TODO compute density and eta from this matrix
+#
+
+
+def plot_phase_transition(self.phase_transition_parameters):
+# Investigate the behaviour of order parameter v_a as a function of density and eta
+fig, ax = plt.subplots()
+plt.title("Order Parameter vs Density")
+plt.xlabel("Density")
+plt.ylabel("Order Parameter")
+plt.plot(self.phase_transition_parameters[:,1],self.phase_transition_parameters[:,0],'-', color = 'r')
+plt.title('Order Parameter vs eta')
+plt.xlabel('Eta')
+plt.ylabel('Order Parameter')
+plt.plot (self.phase_transition_parameters[:,2],self.phase_transition_parameters[:,0],'-',color='b')
+plt.show()
+
+
+# Naming the x-axis, y-axis and the whole graph
+plt.xlabel("Angle")
+plt.ylabel("Magnitude")
+plt.title("Sine and Cosine functions")
+
+# Adding legend, which helps us recognize the curve according to it's color
+plt.legend()
+
+# To load the display window
+plt.show()
+
--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+# from Parameter_variation import *
+
 # import pickle
 # from objects.assembly import Assembly
 # from objects.mesh import Mesh
@@ -33,11 +35,14 @@ class Bird():
     def init_vector(self, seed, N):
         np.random.seed(seed)
         posvector = np.random.rand(N, 2)
-        theta = np.random.uniform(0, 2*np.pi,N)
+        theta = np.random.uniform(0, 2*np.pi,(N,1))
         vector = np.hstack((posvector, theta))
+        vector = vector[np.newaxis,:, :]
         return vector
+
+    """NOTATION: VECTOR[STEP][NBIRD,XYZ]"""
     
-    def __init__(self,seed,vel,N,R,L,eta,dt,Nruns):
+    def __init__(self,seed,vel,N,R,L,eta,dt,Nsteps):
         self.vector = self.init_vector(seed,N)   # This is the NStepsx3 array that stores x,y and theta value at each step
                                                  #At each step, add another layer to the array (in 3D)
         self.velocity = vel                      # Constant norm of velocity for all birds
@@ -61,7 +66,7 @@ class Bird():
         vector_add[:,1] = self.velocity*self.dt*np.sin(theta)
         vector_new = vector_old + vector_add
 
-        for j in range(0, self.N) #can probably be improved using x%L and y%L
+        for j in range(0, self.N):            #can probably be improved using x%L and y%L
             x = vector_new[j,0]
             y = vector_new[j,1]
             if x > L:
@@ -72,30 +77,30 @@ class Bird():
                 vector_new[j,1] = y - L
             if y < 0:
                 vector_new[j,1] = y + L
-        vector_new.reshaped = vector_new.reshape(1, self.N, 3) #reshape the new post matrix in order to be able to concatenate
-        self.vector = np.concatenate((self.vector, vector_new.reshaped), axis=0)
+        vector_new_reshaped = vector_new.reshape(1, self.N, 3) #reshape the new post matrix in order to be able to concatenate
+        self.vector = np.concatenate((self.vector, vector_new_reshaped), axis=0)
 
 
-        vector_new = vector_old[:,0:1] +
+        # vector_new = vector_old[:,0:1] +
 
     def new_theta(self):
         # Function to calculate the average angle of neighbouring boids and updates
-        Neighbours = np.zeros(self.N, self.N)
-        np.fill_diagonal(Neighbours, self.vector[-1][:][2]) #Since every bird is its own neighbour
+        Neighbours = np.zeros((self.N, self.N))
+        np.fill_diagonal(Neighbours, self.vector[-1][:,2]) #Since every bird is its own neighbour
         for i in range(0, self.N):
             # Sigma_theta = self.vector[-1][i][2]  # Sum of neighbours' thetas
             for j in range(i + 1, self.N):
-                if (self.vector[-1][i][0] - self.vector[-1][j][0]) ** 2 + (
-                        self.vector[-1][i][1] - self.vector[-1][j][1]) ** 2 <= self.R ** 2:
-                    Neighbours[i][j] = self.vector[-1][i][2]
-                    Neighbours[j][i] = self.vector[-1][j][2]
+                if (self.vector[-1][i,0] - self.vector[-1][j,0]) ** 2 + (
+                        self.vector[-1][i,1] - self.vector[-1][j,1]) ** 2 <= self.R ** 2:
+                    Neighbours[i,j] = self.vector[-1][i,2]
+                    Neighbours[j,i] = self.vector[-1][j,2]
 
         n = np.count_nonzero(Neighbours, axis=1)
-        avg_theta_r_cos = np.matrix.sum(np.cos(Neighbours), axis=1) / n
-        avg_theta_r_sin = np.matrix.sum(np.sin(Neighbours), axis=1) / n
-        theta_avg_r = np.atan2(avg_theta_r_sin, avg_theta_r_cos) ### WARNING arctan2 gives 0 or pi in the case of two exactly opposite thetas
+        avg_theta_r_cos = np.sum(np.cos(Neighbours), axis=1) / n
+        avg_theta_r_sin = np.sum(np.sin(Neighbours), axis=1) / n
+        theta_avg_r = np.arctan2(avg_theta_r_sin, avg_theta_r_cos) ### WARNING arctan2 gives 0 or pi in the case of two exactly opposite thetas
         # TODO make sure there are no thetas going over 2pi and under 0
-        self.vector[-1][:][2] = theta_avg_r + np.random.uniform(-self.eta / 2, self.eta / 2, size=np.size(theta_avg_r))
+        self.vector[-1][:,2] = theta_avg_r + np.random.uniform(-self.eta / 2, self.eta / 2, size=np.size(theta_avg_r))
 
 
     # def check_transition(self): # we are not sure if this is a good idea, nevertheles we can plot it
@@ -114,16 +119,16 @@ class Bird():
 
     def order_parameter_behaviour(self):
         # Absolut value of the average normslized velocity is the order parameter of the system and checking its behaviour determines the phase transition
-        v_a = np.sqrt((np.matrix.sum(np.cos(self.vector[-1][:][2])))**2 + (np.matrix.sum(np.sin(self.vector[-1][:][2])))**2)/self.N
+        v_a = np.sqrt((np.sum(np.cos(self.vector[-1][:,2])))**2 + (np.sum(np.sin(self.vector[-1][:,2])))**2)/self.N
         return v_a
         #TODO plot v_a as function of rho and eta to see the phase transition
 
     def update(self):
-        for i in range(self.Nruns) #i is the loop variable of the timestep, hard capped at 10 for now
+        for i in range(self.Nsteps):  #i is the loop variable of the timestep, hard capped at 10 for now
             self.evolve()
             self.new_theta()
         #TODO Lastly, check if the dispersion of average theta is close to the noise ;
-        self.va = order_parameter_behaviour()
+        self.va = self.order_parameter_behaviour()
         #if dispersion in range()
         #Timestep = i
         #print(Timestep)
@@ -131,8 +136,9 @@ class Bird():
 
 def make_step(i):
     # swarm.step(dt)
-    line.set_data(swarm.vector[i][:][0], swarm.vector[i][:][1])
+    line.set_data(swarm.vector[i][:,0], swarm.vector[i][:,1])
     return line
+
 
 def animate(vector):
     fig = plt.figure()
@@ -141,13 +147,8 @@ def animate(vector):
     # TODO understand comma?
     ax.set_xlim(0, L)
     ax.set_ylim(0, L)
-    anim = animation.FuncAnimation(fig, make_step(i), interval=1, blit=False)
+    anim = animation.FuncAnimation(fig, make_step, interval=1, blit=False)
 
-def Phase_():
-
-
-
-anim = animation.FuncAnimation(fig, animate, interval=1, blit=False)
 
 if __name__ == '__main__':
     seed = 1
